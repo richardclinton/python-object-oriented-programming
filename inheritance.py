a3a96a6bbcd18030a248eeeeaae82775bd75be9f
richardclinton <richardclinton42@gmail.com>
2023-04-01 13:16:23 +0300

    inheritance definition and its types

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+# Inheritance is the capability of one class to derive or inherit proprties from another class.
+# The class that derives the properties is called the derived class or child class and the class from which properties
+# are being derived is called base class or parent class.
+
+# Types of inheritance 
+# 1. Single inheritance
+#    Single-level inheritance it enables a derived class to inherit characteristics from a single parent class
+# 2. Multilevel inheritance
+#    Multi-level inheritance it enables a derived class to inherit properties from an immediate parent class 
+#    which in turns inherits properties from his parent class.
+# 3. Hierarchical inheritance
+#    Hierarchical level inherintance enables more than one derived class to inherit properties from a parent class
+# 4. Multiple inheritance
+#    Multiple level inheritance enables one derived class to inherit properties from more than one base class.    
