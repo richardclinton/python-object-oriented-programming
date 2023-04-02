0fe51beb76d922702eb743e8cb486f086f83fed8
richardclinton <richardclinton42@gmail.com>
2023-04-02 12:45:56 +0300

    createing object of abstraction

--- abstraction.py
+++ abstraction.py
@@ -14,3 +14,31 @@ class Animal(ABC):
     def sound(self):
         print("This method is for defining the sound by any animal.")
         pass
+    
+class Snake(Animal):
+    def sound(self):
+        print("I can hiss")
+        
+class Dog(Animal):
+    def sound(self):
+        print("I can bark")
+        
+class Lion(Animal):
+    def sound(self):
+        print("I can roar")
+        
+class Cat(Animal):
+    def sound(self):
+        print("I can meow")
+        
+# Our abstratct class has a concrete method sleep() that will be the same for all the child classes.
+# So, we do not define it as an abstract method, thus saving us from code repetition. On the other hand, the
+# sounds that animals make are all different. For that purpose we define the sound() method as an abstract method.
+# We then implement it in all child classes.
+
+cat = Cat()
+cat.sleep()
+cat.sound()
+
+snake = Snake()
+snake.sound()
