c95db60afae4e8d8470711219065480a898957c8
richardclinton <richardclinton42@gmail.com>
2023-04-02 12:19:04 +0300

    abstraction definition

---
+++ abstraction.py
@@ -0,0 +1,3 @@
+# It hide unneccessary code details from the user. Also, when we do not want to give out sensitive parts 
+# of our code implementation and this is where data abstraction came. Data abstraction in python is archieved
+# by through creating abstract classes.
