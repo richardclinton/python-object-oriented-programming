63e9d9a455f1ebbb4dd0c5c8582be542382232ec
richardclinton <richardclinton42@gmail.com>
2023-04-02 12:35:14 +0300

    creating abstract class

--- abstraction.py
+++ abstraction.py
@@ -1,3 +1,16 @@
 # It hide unneccessary code details from the user. Also, when we do not want to give out sensitive parts 
 # of our code implementation and this is where data abstraction came. Data abstraction in python is archieved
 # by through creating abstract classes.
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    
+    # concrete method
+    def sleep(self):
+        print("I am going to sleep in a while")
+       
+    @abstractmethod 
+    def sound(self):
+        print("This method is for defining the sound by any animal.")
+        pass
