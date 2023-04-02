510387ac0b8efde813d68a5626ce8c06bd61be3f
richardclinton <richardclinton42@gmail.com>
2023-04-02 12:03:57 +0300

    encapsulation definition

---
+++ encapsulation.py
@@ -0,0 +1,4 @@
+# It describes the idea of wrapping data and the methods that work on data within one unit.
+# This puts restrictions on accessing variables and methods directly and can prevent accidental modification
+# of data. To prevent accidental change, an object's variable can only be changed by an object's method. Those
+# types of variables are known as private variables.
