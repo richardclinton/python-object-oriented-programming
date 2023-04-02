99bf302a1e6dbc0d7c97d048d8c12c41a39ea1e9
richardclinton <richardclinton42@gmail.com>
2023-04-02 12:07:47 +0300

    encapsulation implementation

--- encapsulation.py
+++ encapsulation.py
@@ -2,3 +2,17 @@
 # This puts restrictions on accessing variables and methods directly and can prevent accidental modification
 # of data. To prevent accidental change, an object's variable can only be changed by an object's method. Those
 # types of variables are known as private variables.
+
+class Base:
+    
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self.__c = "GeeksforGeeks"
+        
+        
+class Derived(Base):
+    
+    def __init__(self):
+        super().__init__()
+        print("Calling private member of the of base class.")
+        print(self.__c)
